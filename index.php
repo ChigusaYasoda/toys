fef985ef55150962c6fd3225cfb078b6b3859768
ChigusaYasoda <chigusa.yasoda@morrighan.biz>
2023-07-06 13:50:59 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,424 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">   <!-- IEの互換表示無効 -->
+<meta name="format-detection" content="telephone=no">   <!-- 電話番号自動リンク無効 -->
+<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE">   <!-- Skypeによる電話番号置換無効 -->
+<meta name="viewport" content="width=device-width">
+<meta name="description" content="<?=SEO_DESCRIPTION?>">
+<meta name="keywords" content="<?=SEO_KEYWORDS?>">
+<title><?=SEO_TITLE?></title>
+<link rel="stylesheet" href="./assets/css/normalize.css">
+<link rel="stylesheet" href="./assets/css/default.css">
+<!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"> -->   <!-- Font Awesome BootstrapCDN -->
+<link rel="stylesheet" href="./assets/css/utility.css">
+<link rel="stylesheet" href="./assets/css/base.css">
+<link rel="stylesheet" href="./assets/css/top.css">
+<link rel="stylesheet" href="./assets/css/slick.css">
+<link rel="stylesheet" href="./assets/css/slick-theme.css">
+</head>
+<body class="home page-top">
+<!-- wrapper -->
+<section id="wrapper">
+  <!-- header -->
+  <header id="cm-header">
+      <div class="cm-header-inner">
+          <div class="flex-jus-between flex-align-item-center">
+              <div class="logo">
+                  <a href="./">
+                      <img src="./assets/images/common/logo.jpg" alt="医療法人浩治会">
+                  </a>
+              </div>
+              <div class="cm-header-menu">
+                  <div class="gnav">
+                      <ul class="flex">
+                          <li>
+                              <a href="./about/">私たちについて</a>
+                          </li>
+                          <li class="dropmenu">
+                              <a href="./">事業内容</a>
+                              <div class="sub-menu">
+                                  <div class="sub-menu-inner">
+                                      <div class="title">事業内容</div>
+                                      <ul>
+                                          <li><a href="./nursing/"><span class="ico01"></span>介護施設事業</a></li>
+                                          <li><a href="./homecare/"><span class="ico02"></span>在宅事業</a></li>
+                                          <li><a href="./list/"><span class="ico04"></span>施設紹介一覧</a></li>
+                                          <li><a href="./training/"><span class="ico03"></span>介護を学ぶ</a></li>
+                                      </ul>
+                                  </div>
+                              </div>
+                          </li>
+                          <li>
+                              <a href="./recruit/">採用情報</a>
+                          </li>
+                          <li>
+                              <a href="./topics/">お知らせ</a>
+                          </li>
+                          <li>
+                              <a href="./entry/">お問い合わせ</a>
+                          </li>
+                      </ul>
+                  </div>
+                  <div class="trigger-menu">
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                  </div>
+              </div>
+          </div>
+          <div class="arrow-left-right"></div>
+      </div>
+  </header>
+  <!-- mainvisual -->
+  <div id="mainvisual">
+      <div class="mainvisual-slider slider-inner">
+          <div class="slider">
+              <div class="slider-item slider01"></div>
+          </div>
+          <div class="slider">
+              <div class="slider-item slider02"></div>
+          </div>
+          <div class="slider">
+              <div class="slider-item slider03"></div>
+          </div>
+      </div>
+      <div class="mainvisual-news">
+          <div class="inner">
+              <a href="./" class="flex-jus-between flex-align-item-center">
+                  <div class="img"><img src="./assets/images/mv-new01.jpg" alt="私たちと一緒に働きませんか？"></div>
+                  <div class="ct-txt">
+                      <div class="title txt-linepink"><span>私たちと一緒に</span><br><span>働きませんか？</span></div>
+                      <p class="btn">採用情報はこちら <span class="arrow"></span></p>
+                  </div>
+              </a>
+              <div class="arrow-left-right"></div>
+          </div>
+      </div>
+  </div>
+  <!-- main -->
+  <main class="cm-main">
+      <!-- About us -->
+      <div class="sec-about-us">
+          <h2 class="title"><strong>私たちについて</strong><span>ABOUT US</span></h2>
+          <div class="about-list">
+              <div class="about-inner">
+                  <div class="ct-txt">
+                      <div class="box">
+                          <div class="box-title">
+                              <h3 class="txt-linepink"><span>いつまでもずっと、住み慣れた環境で。</span><br><span>⼼に寄り添う介護と医療で、</span><br><span>⾃分らしい暮らしをサポートします。</span></h3>
+                              <div class="arrow-left-right"></div>
+                          </div>
+                          <p>私たち浩治会は、<br>安心と笑顔があふれる穏やかな毎日を過ごしていただけるような場所、そして一人ひとりのこれからを見つめながら、医療と介護の専門家たちが力を合わせてサポートする場所をめざして日々取り組んでいます。</p>
+                      </div>
+                  </div>
+                  <div class="img img-linepink">
+                      <img src="./assets/images/img01.jpg" alt="私たちについて">
+                  </div>
+              </div>
+              <div class="about-inner">
+                  <div class="ct-txt">
+                      <div class="box">
+                          <div class="box-title">
+                              <h3 class="txt-linepink"><span>キャッチコピーが入ります。</span><br><span>キャッチコピーが入ります。</span></h3>
+                              <div class="arrow-left-right"></div>
+                          </div>
+                          <p>ダミーのテキストですダミーのテキストですダミーのテキストですダミーのテキストですダミーのテキストですダミーのテキストですダミーのテキストですダミーのテキストですダミーのテキストです</p>
+                      </div>
+                  </div>
+                  <div class="img img-linepink">
+                      <img src="./assets/images/img02.jpg" alt="私たちについて">
+                  </div>
+              </div>
+          </div>
+          <div class="cm-btn-pink">
+              <a href="./about/">私たちについて <span class="arrow"></span></a>
+          </div>
+      </div>
+      <!-- Business -->
+      <div class="sec-business">
+          <div class="container">
+              <h2 class="cm-title"><span>Business</span>事業内容</h2>
+              <div class="business-list">
+                  <div class="business-item">
+                      <h3 class="title"><span class="ico01">介護施設事業</span></h3>
+                      <ul class="flex">
+                          <li>
+                              <a href="./">
+                                  <figure><img src="./assets/images/img-gr01.jpg" alt="大阪城ケアホーム"></figure>
+                                  <div class="ct-txt">
+                                      <p>大阪城ケアホーム</p>
+                                      <span class="arrow02"></span>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="./">
+                                  <figure><img src="./assets/images/img-gr02.jpg" alt="大今里ケアホーム"></figure>
+                                  <div class="ct-txt">
+                                      <p>大今里ケアホーム</p>
+                                      <span class="arrow02"></span>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="./">
+                                  <figure><img src="./assets/images/img-gr03.jpg" alt="大宮ケアホーム光"></figure>
+                                  <div class="ct-txt">
+                                      <p>大宮ケアホーム光</p>
+                                      <span class="arrow02"></span>
+                                  </div>
+                              </a>
+                          </li>
+                      </ul>
+                      <div class="cm-btn-pink">
+                          <a href="./nursing/">一覧へ <span class="arrow"></span></a>
+                      </div>
+                  </div>
+                  <div class="business-item">
+                      <h3 class="title"><span class="ico02">在宅事業</span></h3>
+                      <ul class="flex">
+                          <li>
+                              <a href="./">
+                                  <figure><img src="./assets/images/img-gr04.jpg" alt="グループホームゆめの里"></figure>
+                                  <div class="ct-txt">
+                                      <p>グループホームゆめの里</p>
+                                      <span class="arrow"></span>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="./">
+                                  <figure><img src="./assets/images/img-gr05.jpg" alt="大阪城ケアプランセンター"></figure>
+                                  <div class="ct-txt">
+                                      <p>大阪城ケアプランセンター</p>
+                                      <span class="arrow"></span>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="./">
+                                  <figure><img src="./assets/images/img-gr06.jpg" alt="大今里ケアプランセンター"></figure>
+                                  <div class="ct-txt">
+                                      <p>大今里ケアプランセンター</p>
+                                      <span class="arrow"></span>
+                                  </div>
+                              </a>
+                          </li>
+                      </ul>
+                      <div class="cm-btn-pink">
+                          <a href="./homecare/">一覧へ <span class="arrow"></span></a>
+                      </div>
+                  </div>
+                  <div class="business-item">
+                      <h3 class="title"><span class="ico03">介護を学ぶ</span></h3>
+                      <ul class="flex">
+                          <li>
+                              <a href="./">
+                                  <figure><img src="./assets/images/img-gr07.jpg" alt="大阪城ケア学院(本校)"></figure>
+                                  <div class="ct-txt">
+                                      <p>大阪城ケア学院(本校)</p>
+                                      <span class="arrow02"></span>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="./">
+                                  <figure><img src="./assets/images/img-gr08.jpg" alt="大阪城ケア学院(大今里校)"></figure>
+                                  <div class="ct-txt">
+                                      <p>大阪城ケア学院(大今里校)</p>
+                                      <span class="arrow02"></span>
+                                  </div>
+                              </a>
+                          </li>
+                      </ul>
+                      <div class="cm-btn-pink">
+                          <a href="./training/">一覧へ <span class="arrow"></span></a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!-- Blog -->
+      <div class="sec-blog">
+          <div class="container">
+              <div class="flex-jus-between flex-align-item-center">
+                  <div class="blog-title">
+                      <h2 class="cm-title"><span>Blog</span>ブログ</h2>
+                  </div>
+                  <div class="blog-list">
+                      <ul>
+                          <li>
+                              <a href="./blog/detail/">
+                                  <div class="tag"><span>大阪城ケアホーム</span></div>
+                                  <div class="data">
+                                      <time>2023.03.22</time>
+                                      <p>大阪城ケアホームではイベント食を実施致しました★☆★<br>串カツバイキングです！侍JAPANの応援をしながら目の前で串カツを揚げる実演もあ串カツバイキングです！侍JAPANの応援をしながら目の前で串カツを揚げる実演もあ</p>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="./blog/detail/">
+                                  <div class="tag tag-yellow"><span>大今里ケアホーム</span></div>
+                                  <div class="data">
+                                      <time>2023.03.22</time>
+                                      <p>今回は豪華ホテルのモンブランを提供させていただきました！<br>濃厚なマロンの味を出しながら口当たりはさっぱりで何個でも食べられそうでした。...</p>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="./blog/detail/">
+                                  <div class="tag tag-green"><span>大宮ケアホーム光</span></div>
+                                  <div class="data">
+                                      <time>2023.03.22</time>
+                                      <p>大宮ケアホームでは、3/6にデザートイベントを実施しました！<br>今回は五つ星ホテルザ・リッツ・カールトン大阪のクリームデニッシュを召し上がっ...</p>
+                                  </div>
+                              </a>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+              <div class="cm-btn-pink">
+                  <a href="./blog/">一覧へ <span class="arrow"></span></a>
+              </div>
+          </div>
+      </div>
+      <!-- Recruit -->
+      <div class="sec-recruit">
+          <a href="./recruit/">
+              <h2 class="cm-title">
+                  <span>Recruit</span>採用情報
+                  <div class="arrow-left-right"></div>
+              </h2>
+              <div class="btn">くわしく見る<span class="arrow"></span><div class="arrow-left-right"></div></div>
+          </a>
+      </div>
+      <!-- Topics -->
+      <div class="sec-topics">
+          <div class="container">
+              <div class="flex-jus-between flex-align-item-center">
+                  <div class="blog-title">
+                      <h2 class="cm-title"><span>Topics</span>お知らせ</h2>
+                  </div>
+                  <div class="blog-list">
+                      <ul>
+                          <li>
+                              <a href="./topics/detail/">
+                                  <time>2023.05.01</time>
+                                    <p>【中途・新卒】スタッフ求人情報5/1更新 ダミーのテキストですダミーのテキストですダミーの...</p>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="./topics/detail/">
+                                  <time>2023.03.22</time>
+                                   <p>【大阪城ケアホーム 4/28現在施設利用状況のお知らせ】ダミーのテキストですダミーのテキスト...</p>
+                              </a>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+              <div class="cm-btn-pink">
+                  <a href="./topics/">一覧へ <span class="arrow"></span></a>
+              </div>
+              <ul class="sns">
+                  <li class="facebook">
+                      <a href="./" target="_blank"><span>Facebook</span></a>
+                  </li>
+                  <li class="instagram">
+                      <a href="./" target="_blank"><span>Instagram</span></a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </main>
+  <!-- footer -->
+  <footer id="cm-footer">
+    <div class="container">
+        <div class="logo">
+            <a href="./"><img src="./assets/images/common/logo.jpg" alt="医療法人浩治会"></a>
+        </div>
+        <div class="cm-footer-infor">
+            <div class="title"><span>法人本部</span></div>
+            <div class="ct-txt">
+                <p>〒537-0014<br>大阪市東成区大今里⻄2-17-16 大今里ケアホーム7F</p>
+                <ul class="flex">
+                    <li>TEL <a href="tel:06-6975-1007">06-6975-1007</a></li>
+                    <li>FAX 06-6975-1008</li>
+                </ul>
+            </div>
+        </div>
+        <div class="cm-footer-menu">
+            <div class="item">
+                <p><a href="./about/">私たちについて</a></p>
+            </div>
+            <div class="item">
+                <p><a href="./">事業内容</a></p>
+                <ul>
+                    <li><a href="./nursing/">介護施設事業</a></li>
+                    <li><a href="./homecare/">在宅事業</a></li>
+                    <li><a href="./training/">介護を学ぶ</a></li>
+                </ul>
+            </div>
+            <div class="item">
+                <p><a href="./recruit/">採用情報</a></p>
+                <ul>
+                    <li><a href="./message/">採用メッセージ</a></li>
+                    <li><a href="./evidence/">数字で見る(浩治会)</a></li>
+                    <li><a href="./interview/">社員インタビュー</a></li>
+                    <li><a href="./work/">働く環境</a></li>
+                    <li><a href="./">採用フロー</a></li>
+                    <li><a href="./faq/">よくある質問</a></li>
+                    <li><a href="./recruit/">募集要項一覧</a></li>
+                    <li><a href="./entry/">エントリーフォーム</a></li>
+                </ul>
+            </div>
+            
+            <div class="item">
+                <p><a href="./topics/">お知らせ</a></p>
+                <ul>
+                    <li><a href="./topics/">お知らせ</a></li>
+                    <li><a href="./blog/">ブログ</a></li>
+                </ul>
+                <p class="show_sp mt20"><a href="./company/">会社概要</a></p>
+            </div>
+            <div class="item show_pc">
+                <p><a href="./company/">会社概要</a></p>
+            </div>
+        </div>
+        <div class="cm-footer-links">
+            <ul>
+                <li><a href="./entry/#pp"><span class="arrow"></span>個人情報保護方針</a></li>
+                <li><a href="./"><span class="arrow"></span>一般事業主行動計画・女性の活躍に関する情報</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="page-up">
+        <a href="#wrapper"></a>
+    </div>
+  </footer>
+  <!-- end footer -->
+</section>
+<!-- end wrapper -->
+<!-- <script src="//code.jquery.com/jquery-3.6.3.min.js"></script> -->
+<script>window.jQuery || document.write('<script src="./assets/js/jquery-3.6.3.min.js"><\/script>');</script>   <!-- jQuery Fallback -->
+<!-- end -->
+<script src="./assets/js/script.js"></script>
+<script src="./assets/js/viewport.js"></script>
+<script src="./assets/js/slick.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.slider-inner').slick({
+            dots: true,
+            infinite: true,
+            speed: 1000,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            fade: true,
+            cssEase: 'linear',
+            arrows: false,
+        });
+    });
+</script>
+</body>
+</html>
